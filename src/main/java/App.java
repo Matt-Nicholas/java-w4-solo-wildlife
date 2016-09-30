ff5b7ac1e11a315b098bbf237fd4bf05380669f3
Matt Nicholas <fostermatt82@gmial.com>
2016-09-30 14:01:44 -0700

    add post route to add new animal sighting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,30 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    post("/animal/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      System.out.println("***** name " + name);
+      String location = request.queryParams("location");
+      System.out.println("***** location " + location);
+      String ranger_name = request.queryParams("ranger_name");
+      System.out.println("***** ranger_name " + ranger_name);
+
+      try{
+        if(name.equals("")){
+          throw new UnsupportedOperationException("All feilds must be filled out.");
+        }
+        Animal animal = new Animal(name);
+        Sighting sighting = new Sighting(animal.getId(), location, ranger_name);
+        String newAnimal = animal.getName();
+        model.put("rangerName", sighting.getRangerName());
+        model.put("newAnimal", newAnimal);
+      } catch (java.lang.UnsupportedOperationException e) {
+        String error = "Oops! All feilds must be completed. Sorry for the inconvenience :(";
+        model.put("error", error);
+      }
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
